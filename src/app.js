f023394c8597e9c68362a4a811c19357ad30f7eb
Harshal Shinde <shindeharshal009@gmail.com>
2024-02-19 22:28:28 +0530

    yt subs

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const Subscriber = require("./models/subscribers");
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/subscribers", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/subscribers/names", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find(
+      {},
+      { name: 1, subscribedChannel: 1 }
+    );
+    res.json(subscribers);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/subscribers/:id", async (req, res) => {
+  try {
+    const subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(400).json({ message: "Subscriber not found" });
+    }
+    res.json(subscriber);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = require("./app.js");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const port = 3000;
+
+// Connect to DATABASE
+const DATABASE_URL = "mongodb://localhost/subscribers";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("connected to database"));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Start Server
+app.listen(port, () => console.log(`App listening on port ${port}!`));
